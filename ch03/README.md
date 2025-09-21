fc3ec155c0e1c6c6db4ec39c043ebf40e3fe7219
Kento.Yamada <ymd65536@outlook.com>
2025-09-21 12:24:13 +0000

    Add .gitignore, README updates, and initial project structure for AWS CLI and Bedrock integration

---
+++ ch03/README.md
@@ -0,0 +1,42 @@
+
+# Python仮想環境の有効化
+
+## Linuxの場合
+
+仮想環境を作成します。
+
+```bash
+python -m venv prompt
+```
+
+仮想環境を有効化します。
+
+```bash
+source ./prompt/bin/activate
+```
+
+パッケージをインストールします。
+
+```bash
+pip install -r ./prompt/requirements.txt
+```
+
+## Amazon Bedrockに関すること
+
+
+### model ARNについて
+
+モデルのARNは、以下の形式で指定します。
+
+```
+arn:aws:bedrock:<region>::foundation-model/<model-name>
+```
+
+具体例としては以下のとおりです。
+
+モデルID： anthropic.claude-3-5-sonnet-20240620-v1:0
+モデルARN： arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-3-5-sonnet-20240620-v1:0
+
+モデルIDは以下のドキュメントで確認できます
+
+[Amazon Bedrock でサポートされている基盤モデル - Amazon Bedrock](https://docs.aws.amazon.com/ja_jp/bedrock/latest/userguide/models-supported.html)
---
+++ ch03/sample_script.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from pprint import pprint
+import boto3
+
+
+if TYPE_CHECKING:
+    from mypy_boto3_bedrock_runtime.client import BedrockRuntimeClient # pyright: ignore[reportMissingImports]
+
+bedrock_runtime_client: "BedrockRuntimeClient" = boto3.client(
+    service_name="bedrock-runtime",
+    region_name="us-west-2",
+)
+
+
+if __name__ == "__main__":
+    try:
+        response = bedrock_runtime_client.converse(
+            modelId="anthropic.claude-3-sonnet-20240229-v1:0",
+            system=[
+                {
+                    "text": "あなたは愉快なチャットbotです。<response></response>タグで返答をします。"
+                }
+            ],
+            messages=[{"role": "user", "content": [{"text": "こんにちは!"}]}],
+            inferenceConfig={
+                "temperature": 0.1,
+                "maxTokens": 512,
+                "stopSequences": ["</response>"],
+            },
+        )
+        pprint(response)
+    except Exception as e:
+        print(f"Error: {e}")
